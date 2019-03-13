a5baeddf702f7f2498c3898c9b4342dc496112d4
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-13 09:58:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,3 +16,23 @@ class Dog
     @breed = all_breads
   end
 end
+
+
+class Person
+  
+  def name
+    @name
+  end
+  
+  def name=(all_names)
+    @name = all_names
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(all_jobs)
+    @job = all_jobs
+  end
+end
