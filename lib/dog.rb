321870bf18b391c8856b413ffefba18a8e72e1b2
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-13 09:50:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
 class Dog
-  def initialize(dogs_name)
-    @dogs_name = dogs_name
-  end
-  
-  def name=(full_dogs_name)
-    dogs_name = full_dogs_name
-    @dogs_name = dogs_name
-  end
   
   def name 
     @dogs_name
+  end
+  
+   def name=(full_dogs_name)
+    dogs_name = full_dogs_name
+    @dogs_name = dogs_name
   end
 end
