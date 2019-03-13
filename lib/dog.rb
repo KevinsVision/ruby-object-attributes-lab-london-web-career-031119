aa74524f860676b5b09fe89130874f2b92a9f823
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-13 09:35:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,12 @@ class Dog
     @dogs_name = name
   end
   
-  def name 
-    "#{@dogs_name}"
-  end
-  
   def name=(full_dogs_name)
     dogs_name = full_dogs_name
     @dogs_name = dogs_name
+  end
+  
+  def name 
+    "#{@dogs_name}"
   end
 end
