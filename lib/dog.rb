a645c6357c4b56a76d4f1c5741b0c9a1e206901a
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-13 09:48:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,14 @@
-class Person
- 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+class Dog
+  def initialize(dogs_name)
+    @dogs_name = dogs_name
   end
- 
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
+  
+  def name=(full_dogs_name)
+    dogs_name = full_dogs_name
+    @dogs_name = dogs_name
   end
- 
-  def name
-    "#{@first_name} #{@last_name}".strip
+  
+  def name 
+    @dogs_name
   end
- 
 end
